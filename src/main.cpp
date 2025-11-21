67ced57f40cd128860fd69bb79ad3754070c870c
shad0wshayd3 <2724172+shad0wshayd3@users.noreply.github.com>
2025-11-20 22:25:19 -0700

    feat: version 2.0.0

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,38 @@
-namespace Hook
+namespace Hooks
 {
+	class hkQuitGame :
+		public REX::Singleton<hkQuitGame>
+	{
+	private:
+		static bool QuitGame()
+		{
+			if (auto ConsoleLog = RE::ConsoleLog::GetSingleton())
+			{
+				ConsoleLog->PrintLine("Bye.");
+			}
+
+			std::thread(
+				[]()
+				{
+					std::this_thread::sleep_for(std::chrono::milliseconds(2000));
+					if (auto Main = RE::Main::GetSingleton())
+					{
+						Main->quitGame = true;
+					}
+				})
+				.detach();
+
+			return true;
+		}
+
+	public:
+		static void install()
+		{
+			static REL::Relocation target{ REL::ID(2205365) };
+			target.replace_func(0x29, QuitGame);
+		}
+	};
+
 	class hkShutdown :
 		public REX::Singleton<hkShutdown>
 	{
@@ -11,10 +44,31 @@ namespace Hook
 
 		inline static REL::Hook Hook{ REL::ID(4812562), 0x20, Shutdown };
 	};
+
+	static void Install()
+	{
+		hkQuitGame::install();
+	}
+}
+
+namespace
+{
+	void MessageCallback(F4SE::MessagingInterface::Message* a_msg)
+	{
+		switch (a_msg->type)
+		{
+		case F4SE::MessagingInterface::kPostLoad:
+			Hooks::Install();
+			break;
+		default:
+			break;
+		}
+	}
 }
 
 F4SE_PLUGIN_LOAD(const F4SE::LoadInterface* a_f4se)
 {
-	F4SE::Init(a_f4se, { .trampoline = true });
+	F4SE::Init(a_f4se, { .trampoline = true, .trampolineSize = 14 });
+	F4SE::GetMessagingInterface()->RegisterListener(MessageCallback);
 	return true;
 }
